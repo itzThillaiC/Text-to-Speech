f9b8da7abea5be828eb009ee1db5caddfdae0d41
itzThillaiC <79466435+itzThillaiC@users.noreply.github.com>
2022-07-03 09:11:17 +0530

    imports done

--- tts.py
+++ tts.py
@@ -1,3 +1,23 @@
+
+# Importing Libraries
+from logging import root
 from tkinter import *
 from gtts import gTTS
 from playsound import playsound
+
+# Initializing Window metrics
+root = Tk()
+root.geometry("400*350")
+root.configure(bg='white')
+root.title("TC's Text-to-Speech")
+
+Label(root, text="TEXT_TO_SPEECH", font="arial 30 bold", bg='white smoke').pack()
+Label(text="DataFlair", font='arial 20 bold',
+      bg='white smoke', width='20').pack(side='bottom')
+
+Txt = StringVar()
+Label(root, text="Enter Text", font='arial 20 bold',
+      bg='white smoke').place(x=20, y=60)
+
+entry_field = Entry(root, textvariable=Txt, width='50')
+entry_field.place(x=20, y=100)
